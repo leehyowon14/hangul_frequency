16c47587ad410b63c6522dba86825c13166cdbbc
leehyowon14 <kingtv347@gmail.com>
2023-12-10 16:36:11 +0900

    실시간으로 처리하도록 하여 속도 및 메모리 최적화

--- main.py
+++ main.py
@@ -5,17 +5,11 @@ from hgtk.checker import has_batchim
 from tqdm import tqdm
 
 print("loading...")
-with open('./data/ko_dataset_2.json', 'r', encoding="UTF8") as datas:
-    CONVERSATION_DATA: str = ''
-    for data in tqdm(load(datas)):  # CONVERSATION_DATA에 대화 데이터 추가
-        for conversation in data["conversations"]:
-            CONVERSATION_DATA += conversation["value"]
 
-    # 받침자를 가진 한글을 제외한 모든 문자 제거.
-    print("Filtering is started.")
-    CONVERSATION_DATA = regex_replace('[^각-힣]', '', CONVERSATION_DATA)
-
-print(f"Complete.\nThe length of data is {len(CONVERSATION_DATA)}")
+counter_total: int = 0  # 총 글자 수
+counter_letter: int = 0  # 받침이 있는 글자의 수
+counter_same: int = 0  # 초성과 종성이 같은 글자의 수
+counter_reverse: int = 0  # 초성과 종성이 역가나다순인 글자의 수
 
 # 한글 자음
 HANGUL_CONSONANTS: list[str] = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ',
@@ -27,7 +21,6 @@ GEMINATION_CLUSTERS: dict[str, str] = {"ㄲ": "ㄱ", "ㄸ": "ㄷ", "ㅃ": "ㅂ",
 CONSONANT_CLUSTERS: dict[str, str] = {"ㄺ": "ㄱ", "ㄻ": "ㅁ", "ㄼ": "ㅂ", "ㄽ": "ㅅ",
                                  "ㄾ": "ㅌ", "ㄿ": "ㅍ", "ㅀ": "ㅎ", "ㄳ": "ㅅ",
                                  "ㄵ": "ㅈ", "ㄶ": "ㅎ", "ㅄ": "ㅂ"}
-
 
 def cut_out(target_list: list, index: int) -> list:
     """주어진 리스트에서 한 요소를 제외한 요소를 리턴합니다
@@ -42,40 +35,48 @@ def cut_out(target_list: list, index: int) -> list:
     # if len(target_list)-1 < index: return target_list
     return target_list[:index] + target_list[index+1:]
 
+datas =  open('./data/ko_dataset_2.json', 'r', encoding="UTF8")
+for data in tqdm(load(datas)):  # CONVERSATION_DATA에 대화 데이터 추가
+    conversation_data: str = ''
+    for conversation in data["conversations"]:
+        conversation_data += conversation["value"]
+    
+    conversation_data = regex_replace('[^각-힣]', '', conversation_data)
 
-counter_letter: int = 0  # 받침이 있는 글자의 수
-counter_same: int = 0  # 초성과 종성이 같은 글자의 수
-counter_reverse: int = 0  # 초성과 종성이 역가나다순인 글자의 수
+    for letter in conversation_data:
+        counter_total += 1
+        # 받침 없을 시 다음 루프로
+        if not has_batchim(letter):
+            continue
 
-print("Frequency Counter is Now Started")
-for letter in tqdm(CONVERSATION_DATA):
-    # 받침 없을 시 다음 루프로
-    if not has_batchim(letter):
-        continue
+        # 받침이 존재하는 글자에 한해서 실행
+        consonants:list[str] = cut_out(list(decompose(letter)), 1)  # 초성과 종성만 변수로 로드
 
-    # 받침이 존재하는 글자에 한해서 실행
-    consonants:list[str] = cut_out(list(decompose(letter)), 1)  # 초성과 종성만 변수로 로드
+        # 초성자가 각자병서자일 시 병서자가 아닌 자음으로 교체
+        consonants[0] = GEMINATION_CLUSTERS.get(consonants[0], consonants[0])
 
-    # 초성자가 각자병서자일 시 병서자가 아닌 자음으로 교체
-    consonants[0] = GEMINATION_CLUSTERS.get(consonants[0], consonants[0])
+        # ㄲ, ㅆ 받침 예외처리 (각각 키가 할당되어 있기에 순서의 영향을 받지 않음.)
+        if (consonants[1] == "ㄲ" or
+            consonants[1] == "ㅆ"):
+            continue
 
-    # ㄲ, ㅆ 받침 예외처리 (각각 키가 할당되어 있기에 순서의 영향을 받지 않음.)
-    if (consonants[1] == "ㄲ" or
-        consonants[1] == "ㅆ"):
-        continue
+        # 종성자가 합용병서자일 시 병서자가 아닌 자음으로 교체.
+        # 준세벌식 키보드에서 종성자는 초성과 종성이 동일한 경우를 제외하고 'ㄲ'이나 'ㅆ'이 아닌 각자병서자일 수 없음.
+        consonants[1] = CONSONANT_CLUSTERS.get(consonants[1], consonants[1])
 
-    # 종성자가 합용병서자일 시 병서자가 아닌 자음으로 교체.
-    # 준세벌식 키보드에서 종성자는 초성과 종성이 동일한 경우를 제외하고 'ㄲ'이나 'ㅆ'이 아닌 각자병서자일 수 없음.
-    consonants[1] = CONSONANT_CLUSTERS.get(consonants[1], consonants[1])
+        # 카운트
+        counter_letter += 1
+        if consonants != sorted(consonants):
+            counter_reverse += 1
+        elif len(set(consonants)) == 1:
+            counter_same += 1
+        
 
-    # 카운트
-    counter_letter += 1
-    if consonants != sorted(consonants):
-        counter_reverse += 1
-    elif len(set(consonants)) == 1:
-        counter_same += 1
+datas.close()
 
 # 분석 완료 후 출력
+print("\n\n---Final Result---")
+print(f"Complete.\nThe length of data is {counter_total}")
 print(f"letter : {counter_letter}")
 print(f"same : {counter_same}")
 print(f"reversed : {counter_reverse}")
